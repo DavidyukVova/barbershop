c02344801e4e7ba97c25a8f9ae12c4bae3a74e97
DavidyukVova <vovkadavidyuk@gmail.com>
2020-05-27 21:02:38 +0300

    add five section

--- src/components/Hamepage/FiveSection.js
+++ src/components/Hamepage/FiveSection.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import classes from './Homepage.module.css'
 import i18n from '../../i18n';
-import {Col, Container, Row} from "react-bootstrap";
+import {Button, Col, Container, Row} from "react-bootstrap";
+import {withRouter} from "react-router";
 
-export const FiveSection = () => {
+export const FiveSection = ({history}) => {
     return (
-        <div className={classes.fourSection}>
+        <div className={classes.fiveSection}>
             <div className={classes.contentWrap}>
                 <Container fluid={true}>
                     <Row>
@@ -15,9 +16,38 @@ export const FiveSection = () => {
                             </h2>
                         </Col>
                     </Row>
+                    <div className={classes.gallery}>
+                        <Row>
+                            <Col md={6}>
+                                <div className={classes.imageWrap}>
+                                    <img src="/images/galleryPreview/preview1.jpg" alt="preview"/>
+                                </div>
+                            </Col>
+                            <Col md={6}>
+                                <div className={classes.imageWrap}>
+                                    <img src="/images/galleryPreview/preview2.jpg" alt="preview"/>
+                                </div>
+                                <div className={classes.imageWrap}>
+                                    <img src="/images/galleryPreview/preview3.jpg" alt="preview"/>
+                                </div>
+                            </Col>
+                        </Row>
+                    </div>
+                    <Row>
+                        <Col xs={12}>
+                            <Button
+                                size={"lg"}
+                                className={classes.centeredButton}
+                                variant="outline-secondary"
+                                onClick={e => (history.push(`/${i18n.language}/gallery`))}
+                            >
+                                {i18n.t('Go to gallery')}
+                            </Button>
+                        </Col>
+                    </Row>
                 </Container>
             </div>
         </div>
     )
 };
-export default FiveSection
+export default withRouter(FiveSection)
