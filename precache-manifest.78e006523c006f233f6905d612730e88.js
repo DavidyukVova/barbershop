3dad47da4d85211eb02cd15ec5780c3fde7b41fd
DavidyukVova <vovkadavidyuk@gmail.com>
2020-05-29 19:26:09 +0300

    Updates

---
+++ precache-manifest.78e006523c006f233f6905d612730e88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76df6190d44df3e937e2a03b3a9029cc",
+    "url": "/barbershop/index.html"
+  },
+  {
+    "revision": "055b755ba19bd7528dad",
+    "url": "/barbershop/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "12d3b2a1099b1400041d",
+    "url": "/barbershop/static/css/main.87c7b737.chunk.css"
+  },
+  {
+    "revision": "055b755ba19bd7528dad",
+    "url": "/barbershop/static/js/2.beecc9f4.chunk.js"
+  },
+  {
+    "revision": "f576af3254df0679363d3e6ffe804e36",
+    "url": "/barbershop/static/js/2.beecc9f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12d3b2a1099b1400041d",
+    "url": "/barbershop/static/js/main.58146e42.chunk.js"
+  },
+  {
+    "revision": "d261f6d5dfe854b4f1d0",
+    "url": "/barbershop/static/js/runtime-main.ff0561eb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/barbershop/precache-manifest.82a75e54f40e92bbd03892082154a757.js"
+  "/barbershop/precache-manifest.78e006523c006f233f6905d612730e88.js"
 );
 
 self.addEventListener('message', (event) => {
