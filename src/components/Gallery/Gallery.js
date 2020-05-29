59b4ab45401f24470e92212827ed6342042aee50
DavidyukVova <vovkadavidyuk@gmail.com>
2020-05-29 21:35:11 +0300

    add tabs for gallery

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,10 +1,42 @@
 import React from 'react'
 import Layout from "../Layout";
+import classes from './Gallery.module.css'
+import {Col, Container, Nav, Row, Tab} from "react-bootstrap";
+import i18n from '../../i18n';
 
 const Gallery = () => {
     return (
         <Layout>
-            <p>This will be gallery!</p>
+            <div className={classes.galleryBlock}>
+                <Container>
+                    <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+                        <Row className="justify-content-center">
+                            <Col xs={12}>
+                                <Nav className={classes.tabsButtonWrap} variant="pills">
+                                    <Nav.Item>
+                                        <Nav.Link eventKey="first">
+                                            {i18n.t('Photo')}
+                                        </Nav.Link>
+                                    </Nav.Item>
+                                    <Nav.Item>
+                                        <Nav.Link eventKey="second">
+                                            {i18n.t('Video')}
+                                        </Nav.Link>
+                                    </Nav.Item>
+                                </Nav>
+                            </Col>
+                        </Row>
+                        <Tab.Content>
+                                <Tab.Pane eventKey="first">
+                                    <p>11111</p>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="second">
+                                    <p>22222</p>
+                                </Tab.Pane>
+                        </Tab.Content>
+                    </Tab.Container>
+                </Container>
+            </div>
         </Layout>
     )
 };
