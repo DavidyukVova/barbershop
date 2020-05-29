e2c41c9dd04d0456ee1505524950dfe766314a06
DavidyukVova <vovkadavidyuk@gmail.com>
2020-05-29 20:11:40 +0300

    add contacts page data

--- src/components/Hamepage/SixthSection.js
+++ src/components/Hamepage/SixthSection.js
@@ -19,10 +19,10 @@ const SixthSection = ({env}) => {
                     </Row>
                     <div className={classes.contentBlock}>
                         <Row>
-                            <Col xs={5}>
+                            <Col md={5}>
                                 <img src={`${process.env.PUBLIC_URL}/images/contactsSection.jpg`} alt="contacts"/>
                             </Col>
-                            <Col xs={7}>
+                            <Col md={7}>
                                 <div>
                                     <iframe
                                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d20338.109740085063!2d30.52906487440668!3d50.417557997514336!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cf6bf1e14ac7%3A0x6276ea5f5b14bfa2!2zMzJBLCDQsdGD0LsuINCU0YDRg9C20LHRiyDQndCw0YDQvtC00L7QsiwgMzLQkCwg0JrQuNC10LIsIDAyMDAw!5e0!3m2!1sru!2sua!4v1590686374693!5m2!1sru!2sua"
