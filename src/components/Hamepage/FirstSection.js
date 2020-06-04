1e2d286fec536f3e9c096e8b0391ef9c4ffbdaa3
DavidyukVova <vovkadavidyuk@gmail.com>
2020-06-04 19:44:05 +0300

    fix video for homepage

--- src/components/Hamepage/FirstSection.js
+++ src/components/Hamepage/FirstSection.js
@@ -2,6 +2,8 @@ import React from 'react';
 import classes from './Homepage.module.css'
 import i18n from '../../i18n';
 import {Button} from "react-bootstrap";
+import ReactPlayer from "react-player";
+
 
 const FirstSection = () => {
     return (
@@ -14,6 +16,16 @@ const FirstSection = () => {
                 </div>
             </div>
             <div className={classes.shadow}></div>
+            <ReactPlayer
+                className={classes.video}
+                width={'100%'}
+                height={'100%'}
+                playing={true}
+                loop={true}
+                muted={true}
+                controls={false}
+                url="https://davidyukvova.github.io/barbershop/images/gallery/video/video1.mp4"
+            />
         </div>
     )
 };
